62a13a09df94cbe4d738ab3b9a0d25d82ad5ef52
OlgaKapskaya <olikbuko@gmail.com>
2023-01-30 16:57:07 +0300

    fix(v.0.1.6): fix dispatch function

--- src/create-store/createStore.ts
+++ src/create-store/createStore.ts
@@ -3,13 +3,19 @@ import {Action, Reducer} from "../types/types";
 
 export function createStore<S, A extends Action>(reducer: Reducer<S, A>, initialState: S) {
     let state = initialState
+    //@ts-ignore
+    let callSubscriber = () => {}
     return {
         dispatch(action: A) {
             state = reducer(state, action)
+            callSubscriber()
         },
         getState(): S {
             return state
         },
+        subscribe: (observer: () => void) => {
+            callSubscriber = observer
+        }
     }
 }
 
